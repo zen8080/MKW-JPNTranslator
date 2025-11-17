698b7c686a96b7bb54eee80af5a2bab7bf0fadc1
zen8080 <zen.fujii.0807@gmail.com>
2025-11-17 16:17:52 +0900

    Add files via upload

--- api/translate.js
+++ api/translate.js
@@ -1,6 +1,15 @@
 export default async function handler(req, res) {
+  // CORS: プリフライト (OPTIONS) に対応
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   if (req.method !== "POST") {
-    res.setHeader("Allow", "POST");
+    res.setHeader("Allow", "POST, OPTIONS");
     return res.status(405).send("Method Not Allowed");
   }
 
